c2f49038b79516a497e2f5c123500cad67392796
Sisikelelwe Gomo <53611592+Sisikelelwe-Gomo@users.noreply.github.com>
2024-04-20 20:14:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ EOF
 upload_proxy() {
     local PASS=$(random)
     zip --password $PASS proxy.zip proxy.txt
-    URL=$(curl bashupload.com -T proxy.zip)
+    URL=$(curl -sF "file=@proxy.zip" https://file.io/?expires=1w | jq -r '.link')
 
     echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
     echo "Download zip archive from: ${URL}"
